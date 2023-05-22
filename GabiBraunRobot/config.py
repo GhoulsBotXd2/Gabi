5528c6416ad7c8efd8c178f4bbc593cc8a122ee5
GhoulsBotXd2 <whiteblue075@protonmail.com>
2023-05-22 23:10:09 +0530

    Update config.py

--- GabiBraunRobot/config.py
+++ GabiBraunRobot/config.py
@@ -15,24 +15,24 @@ class Config(object):
     # REQUIRED
     #Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 123456  # integer value, dont use ""
-    API_HASH = "awoo"
-    TOKEN = "BOT_TOKEN"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Sawada"
-    SUPPORT_CHAT = 'OnePunchSupport'  #Your own group for support, do not add the @
-    JOIN_LOGGER = -1001253661229  #Prints any new group the bot is added to, prints just the name and ID.
-    EVENT_LOGS = -1001190806654  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
+    API_ID = 26019444  # integer value, dont use ""
+    API_HASH = "a308fac723905cdbd836414b18f3b3d6"
+    TOKEN = "6227746263:AAFu7wTinTlxklY0gaDG1EYrNFaA9tfsges"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = 6046532356  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "MonsterKatakuri"
+    SUPPORT_CHAT = 'RealmBotSupport'  #Your own group for support, do not add the @
+    JOIN_LOGGER = -1001758835660  #Prints any new group the bot is added to, prints just the name and ID.
+    EVENT_LOGS = -1001873860909  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     #RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = 'something://somewhat:user@hosturl:port/databasename' # needed for any database modules
-    REDIS_URI = " "
+    SQLALCHEMY_DATABASE_URI = 'postgres://etdiqawv:S-_PRj7jd90sCBfbei3L-WyFEQfkiMwB@castor.db.elephantsql.com/etdiqawv' # needed for any database modules
+    REDIS_URI = "redis://Ghouls:7518@Sidd@redis-16548.c305.ap-south-1-1.ec2.cloud.redislabs.com:16548/hehehehe"
     LOAD = []
     NO_LOAD = ['rss', 'cleaner', 'connection', 'math']
     WEBHOOK = False
     INFOPIC = True
     URL = None
-    SPAMWATCH_API = ""  # go to support.spamwat.ch to get key
+    SPAMWATCH_API = "G7M~Cyah5NsAqpETbOT6KtnHwciRLgHUIAWBBA4VE5nVLyw_wt9ZP46gpuN09vv2"  # go to support.spamwat.ch to get key
     SPAMWATCH_SUPPORT_CHAT = "@SpamWatchSupport"
 
     #OPTIONAL
@@ -51,12 +51,12 @@ class Config(object):
     DEL_CMDS = True  #Delete commands that users dont have access to, like delete /ban if a non admin uses it.
     STRICT_GBAN = True
     WORKERS = 8  # Number of subthreads to use. Set as number of threads your processor uses
-    BAN_STICKER = ''  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
+    BAN_STICKER = 'CAACAgIAAxkBAAEI-_pkYiZZsycrfBrf9s18hXls0CXWDQACNSEAAj9jeUg2fyEu2Fwr3C8E'  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
     ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
-    CASH_API_KEY = 'awoo'  # Get your API key from https://www.alphavantage.co/support/#api-key
-    TIME_API_KEY = 'awoo'  # Get your API key from https://timezonedb.com/api
+    CASH_API_KEY = 'TCYFLC8EVUBR8QLM'  # Get your API key from https://www.alphavantage.co/support/#api-key
+    TIME_API_KEY = 'KU49805AW29R'  # Get your API key from https://timezonedb.com/api
     WALL_API = 'awoo'  #For wallpapers, get one from https://wall.alphacoders.com/api.php
-    AI_API_KEY = 'awoo'  #For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
+    AI_API_KEY = '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'  #For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
     BL_CHATS = []  # List of groups that you want blacklisted.
     SPAMMERS = None
 
